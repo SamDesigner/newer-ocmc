bb20721043a3e9414b0de2586d8c4242843a639b
SamDesigner <olabodesamuel08@gmail.com>
2023-11-11 19:59:45 +0100

    Another

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
       </div>
   
       <Ad class="overflow-x-hidden" />
-      <Partners class="overflow-x-hidden" />
+      <!-- <Partners class="overflow-x-hidden" /> -->
 
       <!-- <AboutSection class="overflow-x-hidden" /> -->
       <Insights class="overflow-x-hidden" />
